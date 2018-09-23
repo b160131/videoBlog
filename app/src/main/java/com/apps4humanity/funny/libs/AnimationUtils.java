ed7cbba60a60379a1921db72f67f889a33832a66
Prashant <32607142+b160131@users.noreply.github.com>
2018-09-23 19:12:18 +0530

    Update AnimationUtils.java

--- app/src/main/java/com/apps4humanity/funny/libs/AnimationUtils.java
+++ app/src/main/java/com/apps4humanity/funny/libs/AnimationUtils.java
@@ -4,9 +4,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.support.v7.widget.RecyclerView;
 
-/**
- * Created by vedant on 3/31/2018.
- */
+
 
 public class AnimationUtils {
     public static void animate(RecyclerView.ViewHolder holder, boolean goesDown){
